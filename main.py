998561fc16f1ed308f1de18071464ac71e0dccec
mahmoud <rnahrnoud@gmail.com>
2025-05-15 22:55:12 +0300

    Code Enhancements

--- main.py
+++ main.py
@@ -449,8 +449,8 @@ if __name__ == "__main__":
     parser.add_argument("--num_workers", type=int, default=2, help="Number of dataloader workers")
 
     # Evaluation parameters (new)
-    parser.add_argument("--eval_steps", type=int, default=500, help="Evaluate every N steps")
-    parser.add_argument("--save_steps", type=int, default=1000, help="Save checkpoint every N steps")
+    parser.add_argument("--eval_steps", type=int, default=200, help="Evaluate every N steps")
+    parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every N steps")
     parser.add_argument("--max_checkpoints", type=int, default=2, help="Maximum number of checkpoints to keep")
 
     # Dataset parameters
